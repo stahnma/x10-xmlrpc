5e3df306c725e7e423cf22db751538bc21848145
Michael Stahnke <mastahnke@yahoo.com>
2008-12-11 21:36:37 -0600

    Not really sure how to add unit testing.  If you can't see the X10 target, full testing is difficult.
    Either way, prototype works.

--- tests-xmlrpc-server.rb
+++ tests-xmlrpc-server.rb
@@ -1,5 +1,33 @@
 #!/usr/bin/env ruby
 #
+#
+#
+#Test Cases
+#  x10.remote.on with a valid argument
+#  an invalid argument
+#  too many arguments
+#  too few arguments
+#  
+#  Invalid method name
+#
 
 
-# 
+require 'xmlrpc/client'
+#require 'test/unit'
+
+#class XMLRPC_test < Test::Unit::TestCase
+#  @url="http://localhost:8080"
+ 
+  
+#   def test_fail
+#       assert(false, 'Assertion was false.')
+#   end
+#end
+
+
+url="http://localhost:8080"
+
+puts XMLRPC::Client.new2(url).call("x10.remote.off", 'A1' )
+puts XMLRPC::Client.new2(url).call("x10.remote.on", 'A1' )
+
+
--- xmlrpc-server.rb
+++ xmlrpc-server.rb
@@ -1,4 +1,31 @@
 #!/usr/bin/env ruby
 #
+require "xmlrpc/server"
+
+x10_controller = 'mythtv.stahnkage.com'
+remote_user = 'stahnma'
+remote_command = '/usr/local/bin/heyu'
+on_command = 'fon'
+off_command = 'foff'
+
+#TODO  swap out heyu for another software that has a better license 
 #
-#  
+s = XMLRPC::Server.new(8080) 
+
+s.add_handler("x10.remote.on") do |address|
+  command = 'ssh ' + remote_user.to_s + '@' + x10_controller.to_s +  ' "' + remote_command.to_s + ' ' + on_command.to_s + ' ' + address.to_s + '"'
+  puts command
+  %x(#{command})
+end
+
+s.add_handler("x10.remote.off") do |address| 
+  command = 'ssh ' + remote_user + '@' + x10_controller +  ' "/usr/local/bin/heyu foff ' + address.to_s + '"'
+  %x(#{command})
+end
+
+s.set_default_handler do |name, *args|
+  raise XMLRPC::FaultException.new(-99, "Method #{name} missing" +
+                                   " or wrong number of parameters!")
+end
+
+s.serve
