aaf0f48971f7fb8c83704008aeec7cd1c9d61064
Michael Stahnke <mastahnke@yahoo.com>
2008-12-11 21:44:47 -0600

    Added lots to the README.  Also, fixed a few debug errors.

--- xmlrpc-server.rb
+++ xmlrpc-server.rb
@@ -1,25 +1,26 @@
 #!/usr/bin/env ruby
-#
+
+# BSD License
+# Copyright 2008 Michael Stahnke and Stahnkage.com
+
 require "xmlrpc/server"
 
 x10_controller = 'mythtv.stahnkage.com'
 remote_user = 'stahnma'
+#TODO  swap out heyu for another software that has a better license 
 remote_command = '/usr/local/bin/heyu'
 on_command = 'fon'
 off_command = 'foff'
 
-#TODO  swap out heyu for another software that has a better license 
-#
 s = XMLRPC::Server.new(8080) 
 
 s.add_handler("x10.remote.on") do |address|
   command = 'ssh ' + remote_user.to_s + '@' + x10_controller.to_s +  ' "' + remote_command.to_s + ' ' + on_command.to_s + ' ' + address.to_s + '"'
-  puts command
   %x(#{command})
 end
 
 s.add_handler("x10.remote.off") do |address| 
-  command = 'ssh ' + remote_user + '@' + x10_controller +  ' "/usr/local/bin/heyu foff ' + address.to_s + '"'
+  command = 'ssh ' + remote_user.to_s + '@' + x10_controller.to_s +  ' "' + remote_command.to_s + ' ' + off_command.to_s + ' ' + address.to_s + '"'
   %x(#{command})
 end
 
