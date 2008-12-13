300fb2122289057c573526317ea1b4f7d98f28b7
Michael Stahnke <mastahnke@yahoo.com>
2008-12-13 00:34:51 -0600

    Doing a commit before I start mucking it all up with the newer and easier client/server model and not using ssh

--- x10-xmlrpcd.sh
+++ x10-xmlrpcd.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-ruby /usr/share/x10-xmlrpc/x10-xmlrpcd & > /dev/null & 
+DAEMON="/usr/share/x10-xmlrpc/x10-xmlrpcd"
+
+if [  ! -f "$DAEMON" ] ; then
+   echo "Daemon file missing"
+   exit 100
+fi
+nohup "ruby $DAEMON "  & 
