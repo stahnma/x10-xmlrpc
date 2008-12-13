37fb4333a1b5d2740a8dd71a72716613b228452f
Michael Stahnke <mastahnke@yahoo.com>
2008-12-13 00:39:43 -0600

    Removed all SSH mentions.

--- x10-xmlrpcd.rb
+++ x10-xmlrpcd.rb
@@ -5,11 +5,8 @@
 
 require "xmlrpc/server"
 
-x10_controller = 'mythtv.stahnkage.com'
-remote_user = 'stahnma'
-ssh_key = '/home/stahnma/.ssh/id_dsa'
 #TODO  swap out heyu for another software that has a better license 
-remote_command = '/usr/local/bin/heyu'
+x10_command = '/usr/local/bin/heyu'
 on_command = 'fon'
 off_command = 'foff'
 
@@ -17,24 +14,20 @@ off_command = 'foff'
 # Ensure that one can ssh from this host and to 
 #  the x10_controller as the remote_user
 #
-# See if this works
-command = "ssh -i #{ssh_key} -l #{remote_user} #{x10_controller} /bin/true &> /dev/null"
-system(command)
-if $? != 0
-   raise "Problem with connecting to #{x10_controller} as #{remote_user}...aborting"
-end
-
-s = XMLRPC::Server.new(8080) 
+#
+#
+#TODO need a variable to bind to local IP address or something
+s = XMLRPC::Server.new(port=8080, host='192.168.1.104') 
 
 s.add_handler("x10.remote.on") do |address|
-  command = 'ssh ' + remote_user.to_s + '@' + x10_controller.to_s +  ' "' + remote_command.to_s + ' ' \
-      + on_command.to_s + ' ' + address.to_s + '"'
+  command =x10_command + ' ' + on_command + ' ' + address.to_s + '"'
+  puts command
   %x(#{command})
 end
 
 s.add_handler("x10.remote.off") do |address| 
-  command = 'ssh ' + remote_user.to_s + '@' + x10_controller.to_s +  ' "' + remote_command.to_s + ' ' \
-      + off_command.to_s + ' ' + address.to_s + '"'
+  command =x10_command + ' ' + off_command + ' ' + address.to_s + '"'
+  puts command
   %x(#{command})
 end
 
@@ -42,5 +35,4 @@ s.set_default_handler do |name, *args|
   raise XMLRPC::FaultException.new(-99, "Method #{name} missing" +
                                    " or wrong number of parameters!")
 end
-
 s.serve
