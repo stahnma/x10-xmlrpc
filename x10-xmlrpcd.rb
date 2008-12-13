0031e05131929fd55804b9e8267899428b4cd35d
Michael Stahnke <mastahnke@yahoo.com>
2008-12-13 01:24:08 -0600

    Cleaning up more legacy stuff in the XMLRPC daemon file

--- x10-xmlrpcd.rb
+++ x10-xmlrpcd.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 # BSD License
-# Copyright 2008 Michael Stahnke and Stahnkage.com
+# Copyright 2008 Michael Stahnke
 
 require "xmlrpc/server"
 
@@ -10,21 +10,16 @@ x10_command = '/usr/local/bin/heyu'
 on_command = 'fon'
 off_command = 'foff'
 
-# Ensure that one can ssh from this host and to 
-#  the x10_controller as the remote_user
-#
 #TODO need a variable to bind to local IP address or something
 s = XMLRPC::Server.new(port=8080, host='192.168.1.104') 
 
 s.add_handler("x10.remote.on") do |address|
   command =x10_command + ' ' + on_command + ' ' + address.to_s + '"'
-  puts command
   %x(#{command})
 end
 
 s.add_handler("x10.remote.off") do |address| 
   command =x10_command + ' ' + off_command + ' ' + address.to_s + '"'
-  puts command
   %x(#{command})
 end
 
@@ -32,4 +27,5 @@ s.set_default_handler do |name, *args|
   raise XMLRPC::FaultException.new(-99, "Method #{name} missing" +
                                    " or wrong number of parameters!")
 end
+
 s.serve
